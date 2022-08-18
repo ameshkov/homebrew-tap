c3f14647209792de17da6cbbae6fb455d8a848f8
Andrey Meshkov <am@adguard.com>
2022-08-18 20:41:13 +0300

    Added godnsbench

--- README.md
+++ README.md
@@ -14,4 +14,5 @@ Available packages:
 Package|Description
 ---|---
 [dnslookup](https://github.com/ameshkov/dnslookup)|Simple command line utility to make DNS lookups to the specified server using any protocol (DoH, DoT, DoQ, DNSCrypt)
-[dnslookup](https://github.com/ameshkov/dnscrypt)|DNSCrypt v2 protocol implementation + a command-line tool)
+[dnscrypt](https://github.com/ameshkov/dnscrypt)|DNSCrypt v2 protocol implementation + a command-line tool)
+[godnsbench](https://github.com/ameshkov/godnsbench)|Simple DNS bench util that supports encrypted protocols)
---
+++ godnsbench.rb
@@ -0,0 +1,21 @@
+class Dnslookup < Formula
+    desc "Simple DNS bench util that supports encrypted protocols."
+    homepage "https://github.com/ameshkov/godnsbench"
+    url "https://github.com/ameshkov/godnsbench.git",
+        tag:      "v1.0",
+        revision: "18edc1f3d6c945ea8699b44e464db1eb5e1c95fa"
+    license "MIT"
+  
+    depends_on "go" => :build
+  
+    def install
+      system "go", "build", "-ldflags",
+               "-X main.VersionString=#{version}",
+               *std_go_args
+    end
+  
+    test do
+      assert_match version.to_s, shell_output("#{bin}/godnsbench -v")
+    end
+  end
+  
