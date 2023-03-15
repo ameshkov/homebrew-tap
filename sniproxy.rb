0cb80f838a9264808734596f1296510f2886ddc3
Andrey Meshkov <am@adguard.com>
2023-03-15 13:13:41 +0300

    bump sniproxy to v1.2

--- sniproxy.rb
+++ sniproxy.rb
@@ -2,8 +2,8 @@ class Sniproxy < Formula
     desc "SNI proxy with embedded DNS server that supports blocking and forwarding rules."
     homepage "https://github.com/ameshkov/sniproxy"
     url "https://github.com/ameshkov/sniproxy.git",
-        tag:      "v1.1",
-        revision: "b7c4933461162cce0a5099a04ad8d804ef52c760"
+        tag:      "v1.2",
+        revision: "23ec5407fb38c920f16484c215093745eddf3ade"
     license "Apache-2.0"
   
     depends_on "go" => :build
