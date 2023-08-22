40421d11bb41497aa6f5c7c91880c8f1075633a5
Andrey Meshkov <am@adguard.com>
2023-08-22 12:43:37 +0300

    sniproxy to v1.5.1, godnsbench to v1.8.1

--- godnsbench.rb
+++ godnsbench.rb
@@ -2,8 +2,8 @@ class Godnsbench < Formula
     desc "Simple DNS bench util that supports encrypted protocols."
     homepage "https://github.com/ameshkov/godnsbench"
     url "https://github.com/ameshkov/godnsbench.git",
-        tag:      "v1.8",
-        revision: "8acb17930899a9e2d7ad483d616506013ac83dcc"
+        tag:      "v1.8.1",
+        revision: "3aefe1e78a2673ad22e3439b77a7f61ec39ce4ac"
     license "MIT"
   
     depends_on "go" => :build
--- sniproxy.rb
+++ sniproxy.rb
@@ -2,8 +2,8 @@ class Sniproxy < Formula
     desc "SNI proxy with embedded DNS server that supports blocking and forwarding rules."
     homepage "https://github.com/ameshkov/sniproxy"
     url "https://github.com/ameshkov/sniproxy.git",
-        tag:      "v1.5",
-        revision: "426c940641492539688c6cefc992648271973935"
+        tag:      "v1.5.1",
+        revision: "4885ba054218fc95805b364c74577c152626eb58"
     license "Apache-2.0"
   
     depends_on "go" => :build
