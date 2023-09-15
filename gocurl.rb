98076d8a818c5c8515d7228ac5190e6275cbd91c
Andrey Meshkov <am@adguard.com>
2023-09-15 15:50:39 +0300

    Bump gocurl to v1.0.5

--- gocurl.rb
+++ gocurl.rb
@@ -2,8 +2,8 @@ class Gocurl < Formula
     desc "SNI proxy with embedded DNS server that supports blocking and forwarding rules."
     homepage "https://github.com/ameshkov/gocurl"
     url "https://github.com/ameshkov/gocurl.git",
-        tag:      "v1.0.4",
-        revision: "213c8984901990e34664680215803922a54d1051"
+        tag:      "v1.0.5",
+        revision: "a5c5b2e1708a1f81dd3897fa49e076ea7c14e715"
     license "MIT"
   
     depends_on "go" => :build
