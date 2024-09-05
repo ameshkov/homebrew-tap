629fd4c75865eeb38c155f076ad2b0a4af5beb9f
Andrey Meshkov <am@adguard.com>
2024-09-05 17:58:06 +0300

    Bump godnsbench to v1.9.0

--- godnsbench.rb
+++ godnsbench.rb
@@ -2,8 +2,8 @@ class Godnsbench < Formula
     desc "Simple DNS bench util that supports encrypted protocols."
     homepage "https://github.com/ameshkov/godnsbench"
     url "https://github.com/ameshkov/godnsbench.git",
-        tag:      "v1.8.1",
-        revision: "3aefe1e78a2673ad22e3439b77a7f61ec39ce4ac"
+        tag:      "v1.9.0",
+        revision: "1c6b9b87ea246da0e40e07a622879b423763b027"
     license "MIT"
   
     depends_on "go" => :build
