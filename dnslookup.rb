af0c3ef3f429be5cf8b3600f98b2a7a0a7b7dd31
Andrey Meshkov <am@adguard.com>
2025-12-18 12:35:59 +0300

    Bump dnslookup to v1.11.2

--- dnslookup.rb
+++ dnslookup.rb
@@ -2,21 +2,20 @@ class Dnslookup < Formula
     desc "Simple command-line utility to make DNS lookups using any protocol"
     homepage "https://github.com/ameshkov/dnslookup"
     url "https://github.com/ameshkov/dnslookup.git",
-        tag:      "v1.11.1",
-        revision: "1540652dbb3cb19fae3afa0abc81bdddec3554b8"
+        tag:      "v1.11.2",
+        revision: "6921476799cb3981a83b98b8cb7900b5334da25d"
     license "GPL-3.0-only"
-  
+
     depends_on "go" => :build
-  
+
     def install
       system "go", "build", "-ldflags",
                "-X main.VersionString=#{version}",
                *std_go_args
     end
-  
+
     test do
       assert_match version.to_s, shell_output("#{bin}/dnslookup --version")
       assert_match "dnslookup result", shell_output("#{bin}/dnslookup example.org tls://dns.google")
     end
   end
-  
